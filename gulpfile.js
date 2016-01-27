efff552f951302a1994966971b1fad85613c7466
Anand <anand.seeni@a-cti.com>
2016-01-27 21:56:44 +0530

    changed gulp output file name

--- gulpfile.js
+++ gulpfile.js
@@ -7,12 +7,12 @@ gulp.task('build-lib', function () {
     return browserify('./lib/index.js')
     .require('./lib/index.js', { expose: 'WebRTCUtils'})
     .bundle()
-    .pipe(source('WebRTCUtils.js'))
+    .pipe(source('WebRTCStats.js'))
     .pipe(gulp.dest('./'));
 });
 
-gulp.task('minify', function() {
-  gulp.src('./WebRTCUtils.js')
+gulp.task('minify', ['build-lib'], function() {
+  gulp.src('./WebRTCStats.js')
     .pipe(minify())
     .pipe(gulp.dest('./'));
 });
